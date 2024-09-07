0b0138bb64fc15b63c6f2d8cb71423f3d9e9a45d
Kerwin Naidoo <kerwinjn18@gmail.com>
2024-09-08 01:07:20 +0200

    boomitswiked

--- Assets/Scripts/Enemies/BomberEnemy.cs
+++ Assets/Scripts/Enemies/BomberEnemy.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 using UnityEngine.AI;
+using Unity.VisualScripting;
 
 public class BomberEnemy : Enemy
 {
     [SerializeField] private LineRenderer lineRenderer; // Reference to LineRenderer
     private bool isAttacking = false;
+
+    [SerializeField] private GameObject explosionPF;
 
     private void Awake()
     {
@@ -20,24 +23,19 @@ public class BomberEnemy : Enemy
 
         lineRenderer = GetComponent<LineRenderer>();
 
+        health = 60;
         atkDamage = 100;
         atkDelay = 0;
-        agent.speed = 100;
+        agent.speed = 200;
         agent.stoppingDistance = 0;
     }
 
     void Update()
     {
-        if (target != null)
-        {
-            agent.destination = target.transform.position;
-        }
-        else
-        {
-            target = mainTower;
-        }
+        agent.destination = target.transform.position;
+        
 
-        if (Vector3.Distance(this.transform.position, target.transform.position) < 70)
+        if (Vector3.Distance(this.transform.position, target.transform.position) < 20)
         {
             if (!isAttacking && target != null)
             {
@@ -51,62 +49,20 @@ public class BomberEnemy : Enemy
         }
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Defender"))
-        {
-            target = other.gameObject.GetComponent<Defender>();
-        }
-        else if (other.CompareTag("MainTower"))
-        {
-            target = other.gameObject.GetComponent<Defender>();
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        target = mainTower;
-    }
-
     IEnumerator Attack(Defender target)
     {
         isAttacking = true;
         yield return new WaitForSeconds(atkDelay);
 
-        // Show the attack line for a brief moment
-        if (target != null)
-        {
-            VisualizeAttack(target);
-        }
-
+        
         // Deal damage to the enemy
         target.TakeDamage(atkDamage);
 
-        // Remove the enemy from the queue if it's dead
-        if (target.GetHealth() <= 0)
-        {
-            target = mainTower;
-        }
-
         isAttacking = false;
+
+        Instantiate(explosionPF);
+
+        TakeDamage(atkDamage);        
     }
 
-    private void VisualizeAttack(Defender target)
-    {
-        // Set the positions for the LineRenderer
-        lineRenderer.SetPosition(0, new Vector3(transform.position.x, lineRenderer.GetPosition(0).y, transform.position.z)); // Start at the enemy
-        lineRenderer.SetPosition(1, target.transform.position);    // End at the tower
-
-        // Show the line
-        lineRenderer.enabled = true;
-
-        // Disable the line after a brief delay
-        StartCoroutine(HideLineAfterDelay(0.1f)); // Hide after 0.1 seconds
-    }
-
-    IEnumerator HideLineAfterDelay(float delay)
-    {
-        yield return new WaitForSeconds(delay);
-        lineRenderer.enabled = false;
-    }
 }
