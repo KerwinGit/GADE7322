0ab2d2a5e273da33ee808d0846d4af0d56b73687
Kerwin Naidoo <kerwinjn18@gmail.com>
2024-09-08 15:30:24 +0200

    Spawner spawns enemies

--- Assets/Scripts/Enemies/Spawner.cs
+++ Assets/Scripts/Enemies/Spawner.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject[] enemyPrefabs;
+
+    private GameManager gameManager;
+
+    [SerializeField] private int spawnTotal = 10; //change this to increment with time
+    private float basicProbability;
+    [SerializeField] private float bigProbability;
+    [SerializeField] private float bomberProbability;
+    private float specialProbability;
+
+    private void Awake()
     {
-        
+        specialProbability = bigProbability + bomberProbability;
+
+        basicProbability = 1f - specialProbability;
+
+        Spawn(spawnTotal);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Spawn(int enemies)
     {
-        
+        for (int i = 0; i < enemies; i++)
+        {
+            float randomValue = Random.Range(0f, 1f);
+
+            if (randomValue < basicProbability)
+            {
+                Instantiate(enemyPrefabs[0], transform.position, Quaternion.identity);
+            }
+            else
+            {
+                randomValue = Random.Range(0f, 1f);
+
+                if(randomValue < bigProbability/specialProbability)
+                {
+                    Instantiate(enemyPrefabs[1], transform.position, Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(enemyPrefabs[2], transform.position, Quaternion.identity);
+                }
+            }
+
+        }
     }
 }
