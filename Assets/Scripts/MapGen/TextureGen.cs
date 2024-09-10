26f27e56f36a71343183322f4a12e4b5fcb443ec
Coolguybrad <bnwicks03@gmail.com>
2024-09-10 14:21:50 +0200

    Update TextureGen.cs

--- Assets/Scripts/MapGen/TextureGen.cs
+++ Assets/Scripts/MapGen/TextureGen.cs
@@ -9,6 +9,8 @@ public static class TextureGen  //draws 2d textures
     public static Texture2D ColourTexture(Color[] colourMap, int width, int height)
     {
         Texture2D texture = new Texture2D(width, height);
+        texture.filterMode = FilterMode.Point;
+        texture.wrapMode = TextureWrapMode.Clamp;
         texture.SetPixels(colourMap);
         texture.Apply();
         return texture;
